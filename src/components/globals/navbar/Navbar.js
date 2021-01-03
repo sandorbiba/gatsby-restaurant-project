adb2665749a694d9ca7141482fb6d69c4d10df0b
Sandor Biba <sandorbiba@gmail.com>
2021-01-03 11:41:06 +0100

    add Navbar

--- src/components/globals/navbar/Navbar.js
+++ src/components/globals/navbar/Navbar.js
@@ -1,14 +1,28 @@
-import React from "react"
+import React, { useState } from "react"
 import { NavbarHeader } from "./NavbarHeader"
 import { NavbarIcons } from "./NavbarIcons"
 import { NavbarLinks } from "./NavbarLinks"
+import styled from "styled-components"
 
 export const Navbar = () => {
+  const [navbarOpen, setNavbarOpen] = useState(false)
+
+  const handleNavbar = () => {
+    setNavbarOpen(() => !navbarOpen)
+  }
+
   return (
-    <nav>
-      <NavbarHeader />
-      <NavbarLinks />
+    <NavWraper>
+      <NavbarHeader handleNavbar={handleNavbar} />
+      <NavbarLinks navbarOpen={navbarOpen} />
       <NavbarIcons />
-    </nav>
+    </NavWraper>
   )
 }
+
+const NavWraper = styled.nav`
+  @media (min-width: 768px) {
+    display: flex;
+    align-item: center;
+  }
+`
--- src/components/globals/navbar/NavbarHeader.js
+++ src/components/globals/navbar/NavbarHeader.js
@@ -1,5 +1,37 @@
 import React from "react"
+import { Link } from "gatsby"
+import logo from "../../../images/logo.svg"
+import { FaAlignRight } from "react-icons/fa"
+import styled from "styled-components"
+import { styles } from "../../../utils/index"
 
-export const NavbarHeader = () => {
-  return <div>Header</div>
+export const NavbarHeader = ({ handleNavbar }) => {
+  return (
+    <HeaderWraper>
+      <Link to="/">
+        <img src={logo} alt="company name" />
+      </Link>
+      <FaAlignRight
+        className="toggle-icon"
+        onClick={() => handleNavbar()}
+      ></FaAlignRight>
+    </HeaderWraper>
+  )
 }
+
+const HeaderWraper = styled.div`
+  padding: 0.4rem 1rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  .toggle-icon {
+    font-size: 1.75rem;
+    color: ${styles.colors.mainYellow};
+    cursor: pointer;
+  }
+  @media (min-width: 768px) {
+    .toggle-icon {
+      display: none;
+    }
+  } ;
+`
